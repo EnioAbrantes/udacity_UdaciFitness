a368799716e4e9552447c54ca68058482bc643bb
EnioAbrantes <enio.abrantes.silva@gmail.com>
2019-04-10 16:27:25 -0300

    Adding the styleSheet inside helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { white } from './colors'
+import { red, orange, blue, lightPurp, pink, white } from './colors'
 
 export function getDailyReminderValue () {
-    return {
-      today: "👋 Don't forget to log your data today!"
-    }
+  return {
+    today: "👋 Don't forget to log your data today!"
   }
+}
 
-  
+const styles = StyleSheet.create({
+  iconContainer: {
+    padding: 5,
+    borderRadius: 8,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20
+  },
+})
+
 export function getMetricMetaInfo (metric) {
   const info = {
     run: {
@@ -20,10 +31,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: red}]}>
             <MaterialIcons
               name='directions-run'
-              color={'black'}
+              color={white}
               size={35}
             />
           </View>
@@ -38,10 +49,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: orange}]}>
             <MaterialCommunityIcons
               name='bike'
-              color={'black'}
+              color={white}
               size={32}
             />
           </View>
@@ -56,10 +67,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: blue}]}>
             <MaterialCommunityIcons
               name='swim'
-              color={'black'}
+              color={white}
               size={35}
             />
           </View>
@@ -74,10 +85,10 @@ export function getMetricMetaInfo (metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
             <FontAwesome
               name='bed'
-              color={'black'}
+              color={white}
               size={30}
             />
           </View>
@@ -92,10 +103,10 @@ export function getMetricMetaInfo (metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: pink}]}>
             <MaterialCommunityIcons
               name='food'
-              color={'black'}
+              color={white}
               size={35}
             />
           </View>
@@ -105,43 +116,49 @@ export function getMetricMetaInfo (metric) {
   }
 
   return typeof metric === 'undefined'
-  ? info
-  : info[metric]
+    ? info
+    : info[metric]
 }
 
+
 export function isBetween (num, x, y) {
-if (num >= x && num <= y) {
-  return true
+  if (num >= x && num <= y) {
+    return true
+  }
+
+  return false
 }
-return false
-}
+
 export function calculateDirection (heading) {
-let direction = ''
-if (isBetween(heading, 0, 22.5)) {
-  direction = 'North'
-} else if (isBetween(heading, 22.5, 67.5)) {
-  direction = 'North East'
-} else if (isBetween(heading, 67.5, 112.5)) {
-  direction = 'East'
-} else if (isBetween(heading, 112.5, 157.5)) {
-  direction = 'South East'
-} else if (isBetween(heading, 157.5, 202.5)) {
-  direction = 'South'
-} else if (isBetween(heading, 202.5, 247.5)) {
-  direction = 'South West'
-} else if (isBetween(heading, 247.5, 292.5)) {
-  direction = 'West'
-} else if (isBetween(heading, 292.5, 337.5)) {
-  direction = 'North West'
-} else if (isBetween(heading, 337.5, 360)) {
-  direction = 'North'
-} else {
-  direction = 'Calculating'
+  let direction = ''
+
+  if (isBetween(heading, 0, 22.5)) {
+    direction = 'North'
+  } else if (isBetween(heading, 22.5, 67.5)) {
+    direction = 'North East'
+  } else if (isBetween(heading, 67.5, 112.5)) {
+    direction = 'East'
+  } else if (isBetween(heading, 112.5, 157.5)) {
+    direction = 'South East'
+  } else if (isBetween(heading, 157.5, 202.5)) {
+    direction = 'South'
+  } else if (isBetween(heading, 202.5, 247.5)) {
+    direction = 'South West'
+  } else if (isBetween(heading, 247.5, 292.5)) {
+    direction = 'West'
+  } else if (isBetween(heading, 292.5, 337.5)) {
+    direction = 'North West'
+  } else if (isBetween(heading, 337.5, 360)) {
+    direction = 'North'
+  } else {
+    direction = 'Calculating'
+  }
+
+  return direction
 }
-return direction
-}
+
 export function timeToString (time = Date.now()) {
-const date = new Date(time)
-const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
-return todayUTC.toISOString().split('T')[0]
+  const date = new Date(time)
+  const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
+  return todayUTC.toISOString().split('T')[0]
 }
