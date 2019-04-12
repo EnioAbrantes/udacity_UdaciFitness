feb2f13d732c8523b0ee22041d3ad5e21db49f5e
EnioAbrantes <enio.abrantes.silva@gmail.com>
2019-04-12 19:06:47 -0300

    Creating the new tab Live and asking for a permission.

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import { Constants } from 'expo'
 import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
 
 function UdaciStatusBar ({backgroundColor, ...props}) {
   return (
@@ -39,6 +40,13 @@ const RouteConfigs = {
         tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
       }
   },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
+    }
+  }
 };
   
 const TabNavigatorConfig = {
