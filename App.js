9bf956bb23c0d7d2679cf90a67ec639343455472
EnioAbrantes <enio.abrantes.silva@gmail.com>
2019-04-12 20:07:01 -0300

    Adding some notification to remember the user to update the info on the app.

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import EntryDetail from './components/EntryDetail'
 import Live from './components/Live'
+import { setLocalNotification } from './utils/helpers'
 
 function UdaciStatusBar ({backgroundColor, ...props}) {
   return (
@@ -95,6 +96,10 @@ const MainNavigator = createStackNavigator({
 const TabsContainer = createAppContainer(MainNavigator)
 
 export default class App extends React.Component {
+  componentDidMount() {
+    setLocalNotification()
+  }
+  
   render() {
     return (
       <Provider store={createStore(reducer)}>
